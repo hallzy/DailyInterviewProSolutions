7eff6e0325b679d1ffd51bf6575d2f51436216b1
Steven Hall <Hallzy.18@gmail.com>
2019-08-28 20:30:58 -0700

    Re indented the code to 4 spaces

--- 2019/08-august/27/solution.py
+++ 2019/08-august/27/solution.py
@@ -2,39 +2,39 @@
 
 # Definition for singly-linked list.
 class ListNode(object):
-  def __init__(self, x):
-    self.val = x
-    self.next = None
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
-  def addTwoNumbers(self, l1, l2, c = 0):
-    # Fill this in.
+    def addTwoNumbers(self, l1, l2, c = 0):
+        # Fill this in.
 
-    # Get the value of the given nodes
-    val = l1.val + l2.val + c
+        # Get the value of the given nodes
+        val = l1.val + l2.val + c
 
-    # Get a carry if one exists
-    carry = val // 10
+        # Get a carry if one exists
+        carry = val // 10
 
-    # Get the value without the carry
-    val = val % 10
+        # Get the value without the carry
+        val = val % 10
 
-    # Result of this node is now val
-    result = ListNode(val)
+        # Result of this node is now val
+        result = ListNode(val)
 
-    # If there is nothing to do, then return the last node
-    # Nothing to do if there is no next node, and there is no carry
-    if l1.next == None and l2.next == None and carry == 0:
-        return result;
+        # If there is nothing to do, then return the last node
+        # Nothing to do if there is no next node, and there is no carry
+        if l1.next == None and l2.next == None and carry == 0:
+          return result;
 
-    # l1next and l2next are the next nodes if they exist, otherwise fill them
-    # with a 0 node
-    l1next = l1.next if l1.next != None else ListNode(0)
-    l2next = l2.next if l2.next != None else ListNode(0)
+        # l1next and l2next are the next nodes if they exist, otherwise fill
+        # them with a 0 node
+        l1next = l1.next if l1.next != None else ListNode(0)
+        l2next = l2.next if l2.next != None else ListNode(0)
 
-    # Add the two nodes and carry
-    result.next = self.addTwoNumbers(l1next, l2next, carry)
-    return result
+        # Add the two nodes and carry
+        result.next = self.addTwoNumbers(l1next, l2next, carry)
+        return result
 
 l1 = ListNode(2)
 l1.next = ListNode(4)
@@ -46,6 +46,6 @@ l2.next.next = ListNode(4)
 
 result = Solution().addTwoNumbers(l1, l2)
 while result:
-  print result.val,
-  result = result.next
+    print result.val,
+    result = result.next
 # 7 0 8
--- 2019/08-august/28/solution.py
+++ 2019/08-august/28/solution.py
@@ -3,36 +3,35 @@
 # - Worst case space is if memo fills up to the length of the input string
 
 class Solution:
-  def lengthOfLongestSubstring(self, s):
-    # Fill this in.
+    def lengthOfLongestSubstring(self, s):
+        # Fill this in.
 
+        # The current string
+        memo = {}
 
-    # The current string
-    memo = {}
+        # The length of the longest string we have found
+        longest = 0
 
-    # The length of the longest string we have found
-    longest = 0
+        # Loop through the input string
+        for c in s:
+            # If the character is not in the dictionary, then add it
+            if c not in memo:
+                memo[c] = 1
+                continue
 
-    # Loop through the input string
-    for c in s:
-      # If the character is not in the dictionary, then add it
-      if c not in memo:
-        memo[c] = 1
-        continue
+            # if char was in the memo, then it is a repeat...
 
-      # if char was in the memo, then it is a repeat...
+            # Get the size of the memo
+            length = len(memo)
 
-      # Get the size of the memo
-      length = len(memo)
+            # set the new longest count
+            longest = length if length > longest else longest
 
-      # set the new longest count
-      longest = length if length > longest else longest
+            # reset memo
+            memo = {}
 
-      # reset memo
-      memo = {}
-
-    # return the longest substring length
-    return longest
+        # return the longest substring length
+        return longest
 
 print Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx')
 # 10
